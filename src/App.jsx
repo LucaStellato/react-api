b67ac1e77c5f9ec9f664f1ec7b0f6ff6cb67db1a
LucaStellato <lucastellato435@gmail.com>
2025-11-12 11:48:44 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import { useEffect } from 'react'
+import axios from 'axios'
+
+
+function App() {
+  const [actor, setActor] = useState([])
+  const [actress, setActress] = useState([])
+  const api_server_actor = 'https://lanciweb.github.io/demo/api/actresses/'
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default App
